b90d63265c998d8ad667663e04ff5136c70b4550
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-10-05 19:41:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 17,'male', 'brown', 'feisty', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 1, 'male', 'brown', 'docile', true);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'female', 'black', 'feisty', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 7, 'female', 'black', 'feisty', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'male', 'brown', 'feisty', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 7, 'female', 'black', 'feisty', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'female', 'black', 'feisty', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 7, 'female', 'black', 'feisty', true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 7, 'female', 'black', 'feisty', true);
